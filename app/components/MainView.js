26f405cbfcc65cd4b723b8ed3e825919ab7eea16
Jagiro Elmodovar <jagiro@walla.com>
2017-04-05 20:44:39 +0300

    succsefully imported native-base :)

--- app/components/MainView.js
+++ app/components/MainView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Picker } from 'native-base';
 
 export default class MainView extends React.Component {
     render() {
