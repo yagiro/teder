bb3cbd7f519eb015cec23793ecbc8d177c0d3585
Jagiro Elmodovar <jagiro@walla.com>
2017-04-05 20:41:58 +0300

    created empty react-native project

---
+++ app/components/MainView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class MainView extends React.Component {
+    render() {
+        return(
+            <View>
+                <Text style={{fontSize:50}}>
+                        Hi There.
+                </Text>
+            </View>
+        );
+    }
+}
